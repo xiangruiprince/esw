c6e305ccbc33c1431d6a6c0911476ee285c02651
Xiang Rui <xiangruiprince@gmail.com>
2015-09-15 18:27:04 +0800

    Updated Course page

--- wp-content/themes/academy-child/content-lesson.php
+++ wp-content/themes/academy-child/content-lesson.php
@@ -1,13 +1,25 @@
-<?php ThemexLesson::refresh($GLOBALS['lesson']->ID); ?>
-<div class="lesson-item <?php if($GLOBALS['lesson']->post_parent!=0) { ?>toggle-<?php echo $GLOBALS['lesson']->post_parent; ?> lesson-child<?php } ?> <?php if(ThemexCourse::isMember() && ThemexLesson::$data['progress']!=0) { ?>completed<?php } ?>">
+<?php ThemexLesson::refresh($GLOBALS['lesson']->ID); ?>	
+<div class="lesson-item">
 	<div class="lesson-title">
+
 		<?php if(ThemexCore::checkOption('lesson_collapse') && ($GLOBALS['lesson']->post_parent!=0 || isset(ThemexCourse::$data['lessons'][$GLOBALS['index']+1]) && ThemexCourse::$data['lessons'][$GLOBALS['index']+1]->post_parent!==0)) { ?>
 		<div class="lesson-toggle toggle-element" data-class="toggle-<?php echo $GLOBALS['lesson']->ID; ?>"></div>
 		<?php } ?>
-		<?php if(ThemexLesson::$data['status']=='free') { ?>
-		<div class="course-status"><?php _e('Free', 'academy'); ?></div>
+
+		<div class="course-image">
+			<a href="<?php echo get_permalink(ThemexLesson::$data['ID']); ?>"> <?php echo get_the_post_thumbnail(ThemexLesson::$data['ID'], array(420,420)); ?></a>
+		</div>
+
+		<h3 class="nomargin"><a href="<?php echo get_permalink(ThemexLesson::$data['ID']); ?>" class="<?php if(ThemexLesson::$data['status']=='free') { ?>disabled<?php } ?>"><?php echo get_the_title(ThemexLesson::$data['ID']); ?></a></h3>
+		
+		<?php if ( has_excerpt( ThemexLesson::$data['ID'] ) ) { ?>
+		<p><?php echo nectary_get_the_excerpt(ThemexLesson::$data['ID']); ?></p>
+		<?php } else { ?>
+		<p>Bla bla bla</p>
 		<?php } ?>
-		<h4 class="nomargin"><a href="<?php echo get_permalink(ThemexLesson::$data['ID']); ?>" class="<?php if(ThemexLesson::$data['status']=='free') { ?>disabled<?php } ?>"><?php echo get_the_title(ThemexLesson::$data['ID']); ?></a></h4>
+
+		<?php echo do_shortcode ('[button color="primary" size="small" url="' . get_permalink(ThemexLesson::$data['ID']) .  '" target="self"]進入課程[/button]'); ?>
+
 		<?php if(ThemexCourse::isMember() && !empty(ThemexLesson::$data['quiz']) && !empty(ThemexLesson::$data['progress'])) { ?>
 		<div class="course-progress">
 			<span style="width:<?php echo ThemexLesson::$data['progress']; ?>%;"></span>
--- wp-content/themes/academy-child/functions.php
+++ wp-content/themes/academy-child/functions.php
@@ -5,3 +5,13 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+function nectary_get_the_excerpt($post_id) {
+  global $post;  
+  $save_post = $post;
+  $post = get_post($post_id);
+  $output = get_the_excerpt();
+  $output = wp_trim_words($output, 3, '...');
+  $post = $save_post;
+  return $output;
+}
