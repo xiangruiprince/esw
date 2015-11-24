016c3877eba12074adcc9b0e1d45fa36f23a0679
Xiang Rui <xiangruiprince@gmail.com>
2015-11-24 14:36:02 +0800

    Added new template for Registration template in child to display new custom fields

--- wp-content/themes/academy/framework/strings.php
+++ wp-content/themes/academy/framework/strings.php
@@ -1,4 +1,11 @@
 <?php 
+$strings['age-group-name']=__('Age Group', 'academy');
+$strings['age-group-options']=__('21 - 30, 31 - 40', 'academy');
+$strings['working-industry-name']=__('Working Industry', 'academy');
+$strings['working-industry-options']=__('Gaming, Fashion', 'academy');
+$strings['mobile-number-name']=__('Mobile Number', 'academy');
+$strings['country-of-residence-name']=__('Country of Residence', 'academy');
+$strings['country-of-residence-options']=__('Singapore, Malaysia', 'academy');
 $strings['name-name']=__('Name', 'academy');
 $strings['email-name']=__('Email', 'academy');
 $strings['message-name']=__('Message', 'academy');
